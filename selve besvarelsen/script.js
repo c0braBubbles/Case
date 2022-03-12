2a88aa1864c83f90e3f169662a1ea8369838fae7
c0braBubbles <233566@usn.no>
2022-03-12 23:37:22 +0100

    Tekst går ikke over 2 linjer.

--- selve besvarelsen/script.js
+++ selve besvarelsen/script.js
@@ -1,14 +1,13 @@
 // Dynamiske variabler hentet fra oppgaveteksten
-
-let title = "Dette er H1";   // H1 og H2 kan være alt fra 3 ord til lang tekst
-let pretitle = "Dette er H2";
+let title = "En bedre hverdag? Last ned e-guide for Microsoft Teams";   // H1 og H2 kan være alt fra 3 ord til lang tekst
+let pretitle = "Ikke gå i denne WiFi-fella når du er på jobbreise";
 let Imgurl = "https://www.newwork.no/CaS/NW_sandbox/testbilde.jpg";
 let bordercolor = "#333333";
 let textbox_bgcolor = "#666666";
 let titletxtcolor = "#000000";
 let pretitletxtcolor = "#000000";
 let highlight_color = "#FFFF00";
-let highlight_style = "normal"; // kan være "bold" og "italic"
+let highlight_style = "italic"; // kan være "bold" og "italic"
 
 
 // Henter DOM setter verdier og style fra de dynamiske variablene
@@ -18,11 +17,13 @@ bilde.src = Imgurl;
 var h1 = document.getElementById("h1");
 h1.style.color = titletxtcolor;
 h1.style.fontSize = "30px";
-// h1.innerHTML = title;
+h1.innerHTML = title;       // disse skal kommenteres ut når du er ferdig
 
 var h2 = document.getElementById("h2");
 h2.style.color = pretitletxtcolor;
-// h2.innerHTML = pretitle;
+h2.style.fontSize = "25px";
+h2.style.fontStyle = highlight_style;
+h2.innerHTML = pretitle;    // disse skal kommenteres ut når du er ferdig
 
 var txtDiv = document.getElementById("txtDiv");
 txtDiv.style.backgroundColor = textbox_bgcolor;
@@ -50,7 +51,7 @@ var texts = [
     "I denne leiligheten er det ingen lysbrytere",
 
     "Dårlig bildekvalitet?",
-    "Dette gjør TV-ekspert Trond når han skal importere ny TV", 
+    "Dette gjør TV-ekspert Trond når han skal importere ny TV",
 
     "",
     ""
@@ -66,9 +67,11 @@ setInterval(function () {
 
     if (i == texts.length) {
         i = 0;
+        fitText();
     }
     else {
         i+=2;
+        fitText();
     }
 }, 5000);
 
@@ -79,3 +82,53 @@ $(document).ready(function () {
         $("#testtxt").slideDown("slow");
     });
 });
+
+
+
+function fitText() {
+    $('#h2').each(function (index, element) {
+        var current = $(this);
+        var text = current.text();
+        var words = text.split(' ');
+
+        current.text(words[0]);
+        var height = current.height();
+
+        for (var i = 0; i < words.length; i++) {
+            if (i == 0) {
+                current.text(words[0]);
+            } else {
+                if (current.text(current.text() + ' ' + words[i]).height() > height) {
+                    let fontSize = parseInt(h2.style.fontSize);
+                    h2.style.fontSize = `${fontSize - 1}px`;
+                    fitText();
+                } else {}
+            }
+        }
+    });
+
+    $('#h1').each(function (index, element) {
+        var current = $(this);
+        var text = current.text();
+        var words = text.split(' ');
+
+        current.text(words[0]);
+        var height = current.height();
+
+        for (var i = 0; i < words.length; i++) {
+            if (i == 0) {
+                current.text(words[0]);
+            } else {
+                if (current.text(current.text() + ' ' + words[i]).height() > height * 2) {
+                    let h2Fontsize = parseInt(h2.style.fontSize);
+                    let fontSize = parseInt(h1.style.fontSize);
+
+                    h1.style.fontSize = `${fontSize - 1}px`;
+                    h2.style.fontSize = `${fontSize - 5}px`;
+                    fitText();
+                } else {}
+            }
+        }
+    });
+}
+fitText();
